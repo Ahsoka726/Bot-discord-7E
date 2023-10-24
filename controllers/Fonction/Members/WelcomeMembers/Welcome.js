0da14f7b98431557f92733270cab08befb8d485f
Ahsoka <115781403+Ahsoka726@users.noreply.github.com>
2023-10-24 21:42:21 +0200

    commit reprise bot

--- controllers/Fonction/Members/WelcomeMembers/Welcome.js
+++ controllers/Fonction/Members/WelcomeMembers/Welcome.js
@@ -1,11 +1,22 @@
 const fs = require("fs");
+const { Client, userMention } = require("discord.js");
+const { measureMemory } = require("vm");
+const client = new Client({ intents: 3276799 });
+
 function NouveauMembre(member, client) {
-  const channel = client.channels.cache.get(IdSalonGeneral);
-  channel.send("Bienvenue sur le serveur: ${member.user.tag}");
-  member.send("Voici toutes les infos qui te seront nécessaire"); //en mili secondes 1 minutes= 120000 ms;; // Envoyer un message privé au nouveau membre
+  //Déclaration salon id du serveur
+  const channelId = '733265815406641252';
+  
+  const channel = client.channels.cache.get(channelId);
+
+  //Message d'erreur s'il ne trouve pas l'id du salon
+  if (!channel) return console.error(`Le canal avec l'ID ${channelId} est introuvable.`);
+
+  channel.send( `Bienvenue ${member.user.tag} sur le serveur Star Wars Universe [FR]! Tu peux aller dans <#728698140684845097> pour ajouter tes rôles :grogu_happy: et je t'invite à lire le règlement qui se trouve ici <#728643330191261697>`);
 }
+  
   // Trouver la clé du message correspondant au préfixe de la commande
-  function EnvoieMessagePredef(message, selection) {
+  /*function EnvoieMessagePredef(message, selection) {
     var fs = require("fs");
     const member = message.mentions.members.first();
     const message_string = selection.Contenue.replace("###", member);
@@ -16,8 +27,7 @@ function NouveauMembre(member, client) {
       }, 120000); // 5 minutes en millisecondes
     }); //en mili secondes 1 minutes= 120000 ms;; // Envoyer un message privé au nouveau membre;
     console.log(member.user.tag, selection.Reponse);
-  }
+  }*/
 module.exports = {
-  NouveauMembre,
-  EnvoieMessagePredef
+  NouveauMembre
 };
